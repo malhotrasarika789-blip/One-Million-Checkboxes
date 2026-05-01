0ab4cedc78f18301cf9935790deca0f2defc50e5
malhotrasarika789-blip <malhotrasarika789@gmail.com>
2026-05-01 16:56:45 +0530

    update backend changes

--- src/app.js
+++ src/app.js
@@ -2,11 +2,16 @@ import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 
+// console.log("REDIS_URL:", process.env.REDIS_URL);
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRoutes);
+    app.get("/", (req, res) => {
+    res.send("One Million Checkboxes backend is running 🚀");
+});
 
 export default app;
